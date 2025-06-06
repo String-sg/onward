df83450949fe097df73839d42f8567115d1b54da
YimingIsCOLD <yimingpeh@gmail.com>
2025-06-06 17:40:48 +0800

    feat(learner): set up `app` group to share layout

---
+++ apps/learner/src/routes/(app)/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const { children } = $props();
+</script>
+
+<main class="mx-auto flex h-full w-full max-w-5xl flex-col">
+  {@render children()}
+</main>
---
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -0,0 +1,1 @@
+<div class="px-6">Home page!</div>
