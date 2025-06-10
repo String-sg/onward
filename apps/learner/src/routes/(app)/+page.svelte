58ede82a544756c1d27596f1ae0bc6ed621412e5
Kelly Lim <kellylim.06@gmail.com>
2025-06-10 14:28:34 +0800

    feat(learner): update homepage with multi learning units card

--- apps/learner/src/routes/(app)/+page.svelte
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -1,1 +1,20 @@
-<div class="px-6">Home page!</div>
+<div class="px-6 pt-5">
+  <div class="flex flex-col rounded-3xl bg-purple-100 p-6">
+    <div class="flex flex-wrap gap-1">
+      <div class="rounded-md bg-purple-200 px-2.5 py-0.5">
+        <span class="text-xs font-semibold text-purple-900">Special Education Needs</span>
+      </div>
+    </div>
+
+    <div class="mt-1">
+      <span class="text-xl font-medium text-slate-950">
+        Navigating Special Educational Needs in Singapore: A Path to Inclusion
+      </span>
+    </div>
+    <div class="mt-3 flex gap-x-1">
+      <span class="text-sm text-slate-600">Guidance Branch</span>
+      <span class="text-sm text-slate-600">•</span>
+      <span class="text-sm text-slate-600">2 days ago</span>
+    </div>
+  </div>
+</div>
