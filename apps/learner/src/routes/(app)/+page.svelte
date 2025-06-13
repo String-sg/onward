890c18ff24c013cae9545210140785d9ec5f599c
Kelly Lim <kellylim.06@gmail.com>
2025-06-13 16:16:01 +0800

    chore(learner): rename component from plural to singular

--- apps/learner/src/routes/(app)/+page.svelte
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import MultiLearningUnits from '$lib/components/MultiLearningUnits.svelte';
+  import MultiLearningUnit from '$lib/components/MultiLearningUnit.svelte';
 </script>
 
 <div class="px-6 pt-5">
-  <MultiLearningUnits
+  <MultiLearningUnit
     collection="Special Educational Needs"
     title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
     tags={['Podcast']}
