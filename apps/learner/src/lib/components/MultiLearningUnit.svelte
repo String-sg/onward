ca65ba0387bc1a62e46465b298116587d3f5a358
YimingIsCOLD <yimingpeh@gmail.com>
2025-06-20 07:39:25 +0800

    refactor(learner): replace `mt` with `gap` in `MultiLearningUnit` component

--- apps/learner/src/lib/components/MultiLearningUnit.svelte
+++ apps/learner/src/lib/components/MultiLearningUnit.svelte
@@ -12,33 +12,37 @@
   let { title, tags, isPlayPanelVisible }: Props = $props();
 </script>
 
-<div class="flex flex-col rounded-3xl bg-purple-100 p-6">
-  <div class="flex flex-wrap gap-1">
-    {#each tags as tag (tag.content)}
-      <Badge variant={tag.variant}>{tag.content}</Badge>
-    {/each}
-  </div>
+<div class="flex flex-col gap-y-6 rounded-3xl bg-purple-100 p-6">
+  <div class="flex flex-col gap-y-2">
+    <div class="flex flex-wrap gap-1">
+      {#each tags as tag (tag.content)}
+        <Badge variant={tag.variant}>{tag.content}</Badge>
+      {/each}
+    </div>
 
-  <div class="mt-1">
-    <span class="text-xl font-medium text-slate-950">
-      {title}
-    </span>
-  </div>
+    <div class="flex flex-col gap-y-3">
+      <span class="text-xl font-medium text-slate-950">
+        {title}
+      </span>
 
-  <div class="mt-3 flex gap-x-1">
-    <span class="text-sm text-slate-600">Guidance Branch</span>
-    <span class="text-sm text-slate-600">•</span>
-    <span class="text-sm text-slate-600">2 days ago</span>
+      <div class="flex gap-x-1">
+        <span class="text-sm text-slate-600">Guidance Branch</span>
+        <span class="text-sm text-slate-600">•</span>
+        <span class="text-sm text-slate-600">2 days ago</span>
+      </div>
+    </div>
   </div>
 
   {#if isPlayPanelVisible}
-    <div class="mt-5 flex justify-start gap-3">
-      <button class="flex items-center justify-center gap-2 rounded-full bg-purple-300 px-6 py-4">
+    <div class="flex items-center gap-x-3">
+      <button
+        class="flex cursor-pointer items-center gap-x-2 rounded-full bg-purple-300 px-6 py-4 transition-colors active:bg-purple-400/75"
+      >
         <Play />
-        <span>Play</span>
+        <span class="font-medium text-slate-950">Play</span>
       </button>
 
-      <div class="flex items-center justify-center gap-2">
+      <div class="flex items-center gap-x-2">
         <svg viewBox="0 0 24 24" class="h-6 w-6">
           <circle cx="12" cy="12" r="9" stroke="#D8B4FE" fill="none" stroke-width="3px" />
           <circle
