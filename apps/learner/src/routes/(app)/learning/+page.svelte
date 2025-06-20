59634b32209137a4104bc1345621657b25158691
Kelly Lim <kellylim.06@gmail.com>
2025-06-20 16:04:19 +0800

    chore(learner): refactor to hardcode collections and eliminate mappings

--- apps/learner/src/routes/(app)/learning/+page.svelte
+++ apps/learner/src/routes/(app)/learning/+page.svelte
@@ -2,39 +2,39 @@
   import { HandHeart, MessageCircleQuestion, PersonStanding } from '@lucide/svelte';
 
   import Collection from '$lib/components/Collection.svelte';
-
-  const variantMap = {
-    'Special Educational Needs': 'purple',
-    'Character and Citizenship Education': 'rose',
-    'Inclusive Education': 'amber',
-  } as const;
-
-  const collections = [
-    {
-      title: 'Special Educational Needs',
-      icon: HandHeart,
-      noOfPodcasts: 12,
-      noOfNotes: 2,
-    },
-    {
-      title: 'Character and Citizenship Education',
-      icon: PersonStanding,
-      noOfPodcasts: 8,
-      noOfNotes: 3,
-    },
-    {
-      title: 'Inclusive Education',
-      icon: MessageCircleQuestion,
-      noOfPodcasts: 10,
-      noOfNotes: 1,
-    },
-  ] as const;
 </script>
 
 <div class="grid auto-rows-fr grid-cols-2 gap-4 px-6 pt-5">
-  {#each collections as collection, i (i)}
-    <div class={i === 0 ? 'col-span-2' : ''}>
-      <Collection {...collection} variant={variantMap[collection.title]} />
-    </div>
-  {/each}
+  <!-- Collection 1 -->
+  <div class="col-span-2">
+    <Collection
+      title="Special Educational Needs"
+      icon={HandHeart}
+      noOfPodcasts={12}
+      noOfNotes={2}
+      variant="purple"
+    />
+  </div>
+
+  <!-- Collection 2 -->
+  <div>
+    <Collection
+      title="Character and Citizenship Education"
+      icon={PersonStanding}
+      noOfPodcasts={8}
+      noOfNotes={3}
+      variant="rose"
+    />
+  </div>
+
+  <!-- Collection 3 -->
+  <div>
+    <Collection
+      title="Inclusive Education"
+      icon={MessageCircleQuestion}
+      noOfPodcasts={10}
+      noOfNotes={1}
+      variant="amber"
+    />
+  </div>
 </div>
