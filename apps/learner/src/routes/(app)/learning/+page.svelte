91d30e2722a103a2dc0fb9bc4c6bcef09d53a97a
Kelly Lim <kellylim.06@gmail.com>
2025-06-23 15:29:13 +0800

    feat(learner): remove unnecessary div wrappers in grid layout

--- apps/learner/src/routes/(app)/learning/+page.svelte
+++ apps/learner/src/routes/(app)/learning/+page.svelte
@@ -4,37 +4,31 @@
   import Collection from '$lib/components/Collection.svelte';
 </script>
 
-<div class="grid auto-rows-fr grid-cols-2 gap-4 px-6 pt-5">
+<div class="grid auto-rows-fr grid-cols-2 gap-4 px-6 pt-5 [&>:first-child]:col-span-2">
   <!-- Collection 1 -->
-  <div class="col-span-2">
-    <Collection
-      title="Special Educational Needs"
-      icon={HandHeart}
-      noOfPodcasts={12}
-      noOfNotes={2}
-      variant="purple"
-    />
-  </div>
+  <Collection
+    title="Special Educational Needs"
+    icon={HandHeart}
+    noOfPodcasts={12}
+    noOfNotes={2}
+    variant="purple"
+  />
 
   <!-- Collection 2 -->
-  <div>
-    <Collection
-      title="Character and Citizenship Education"
-      icon={PersonStanding}
-      noOfPodcasts={8}
-      noOfNotes={3}
-      variant="rose"
-    />
-  </div>
+  <Collection
+    title="Character and Citizenship Education"
+    icon={PersonStanding}
+    noOfPodcasts={8}
+    noOfNotes={3}
+    variant="rose"
+  />
 
   <!-- Collection 3 -->
-  <div>
-    <Collection
-      title="Inclusive Education"
-      icon={MessageCircleQuestion}
-      noOfPodcasts={10}
-      noOfNotes={1}
-      variant="amber"
-    />
-  </div>
+  <Collection
+    title="Inclusive Education"
+    icon={MessageCircleQuestion}
+    noOfPodcasts={10}
+    noOfNotes={1}
+    variant="amber"
+  />
 </div>
