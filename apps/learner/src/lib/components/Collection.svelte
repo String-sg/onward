1afb8232f5e37780dfe54d0b618cc60387817798
kellylimmm <54774084+kellylimmm@users.noreply.github.com>
2025-06-23 16:50:28 +0800

    feat(learner): add collection card component (#32)

---
+++ apps/learner/src/lib/components/Collection.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import type { Icon } from '@lucide/svelte';
+
+  type Variant = 'purple' | 'rose' | 'amber';
+
+  interface Props {
+    /**
+     * The title of the Collection.
+     */
+    title: string;
+    /**
+     * The icon component representing the Collection.
+     */
+    icon: typeof Icon;
+    /**
+     * Total number of podcasts in this Collection.
+     */
+    numberofpodcasts: number;
+    /**
+     * Total number of notes in this Collection.
+     */
+    numberofnotes: number;
+    /**
+     * Visual colour theme for the Collection.
+     */
+    variant: Variant;
+  }
+
+  let { title, icon: IconComponent, numberofpodcasts, numberofnotes, variant }: Props = $props();
+</script>
+
+<div
+  class={[
+    'min-h-50 flex h-full flex-col gap-y-2 rounded-3xl p-4',
+    variant === 'purple' && 'bg-purple-500',
+    variant === 'rose' && 'bg-rose-500',
+    variant === 'amber' && 'bg-amber-500',
+  ]}
+>
+  <div class="flex">
+    <div
+      class={[
+        'rounded-lg p-2',
+        variant === 'purple' && 'bg-purple-200 text-purple-900',
+        variant === 'rose' && 'bg-rose-200 text-rose-900',
+        variant === 'amber' && 'bg-amber-200 text-amber-900',
+      ]}
+    >
+      <IconComponent />
+    </div>
+  </div>
+
+  <span class="text-xl font-semibold text-white">{title}</span>
+
+  <div class="flex flex-col">
+    <span class="text-sm text-white">{numberofpodcasts} podcasts</span>
+    <span class="text-sm text-white">{numberofnotes} notes</span>
+  </div>
+</div>
--- apps/learner/src/routes/(app)/learning/+page.svelte
+++ apps/learner/src/routes/(app)/learning/+page.svelte
@@ -1,1 +1,34 @@
-<div class="px-6">Learning page!</div>
+<script lang="ts">
+  import { HandHeart, MessageCircleQuestion, PersonStanding } from '@lucide/svelte';
+
+  import Collection from '$lib/components/Collection.svelte';
+</script>
+
+<div class="grid auto-rows-fr grid-cols-2 gap-4 px-6 pt-5 [&>:first-child]:col-span-2">
+  <!-- Collection 1 -->
+  <Collection
+    title="Special Educational Needs"
+    icon={HandHeart}
+    numberofpodcasts={12}
+    numberofnotes={2}
+    variant="purple"
+  />
+
+  <!-- Collection 2 -->
+  <Collection
+    title="Character and Citizenship Education"
+    icon={PersonStanding}
+    numberofpodcasts={8}
+    numberofnotes={3}
+    variant="rose"
+  />
+
+  <!-- Collection 3 -->
+  <Collection
+    title="Inclusive Education"
+    icon={MessageCircleQuestion}
+    numberofpodcasts={10}
+    numberofnotes={1}
+    variant="amber"
+  />
+</div>
