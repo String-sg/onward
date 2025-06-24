00a879b6452115f712946159f78acace19430a7b
Kelly Lim <kellylim.06@gmail.com>
2025-06-24 12:08:44 +0800

    chore(learner): update component name to be closely tied to each other in alphabetical

--- apps/learner/src/routes/(app)/+page.svelte
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import FullMultiLearningUnit from '$lib/components/FullMultiLearningUnit.svelte';
+  import MultiLearningUnitFull from '$lib/components/MultiLearningUnitFull.svelte';
 </script>
 
 <div class="px-6 pt-5">
-  <FullMultiLearningUnit
+  <MultiLearningUnitFull
     to="/"
     tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
     title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
