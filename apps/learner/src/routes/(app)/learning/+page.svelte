35d94f4483651ab25c438edccee604dac2a0e3e1
Kelly Lim <kellylim.06@gmail.com>
2025-06-25 16:49:16 +0800

    feat(learner): update LJ page to include Your learnings text and `Funnel` and `BrainCircuit` icon

--- apps/learner/src/routes/(app)/learning/+page.svelte
+++ apps/learner/src/routes/(app)/learning/+page.svelte
@@ -1,34 +1,57 @@
 <script lang="ts">
-  import { HandHeart, MessageCircleQuestion, PersonStanding } from '@lucide/svelte';
+  import {
+    BrainCircuit,
+    Funnel,
+    HandHeart,
+    MessageCircleQuestion,
+    PersonStanding,
+  } from '@lucide/svelte';
 
   import Collection from '$lib/components/Collection.svelte';
 </script>
 
-<div class="grid auto-rows-fr grid-cols-2 gap-4 px-6 pt-5 [&>:first-child]:col-span-2">
-  <!-- Collection 1 -->
-  <Collection
-    title="Special Educational Needs"
-    icon={HandHeart}
-    numberofpodcasts={12}
-    numberofnotes={2}
-    variant="purple"
-  />
+<div class="flex flex-col px-6">
+  <div class="flex flex-col gap-6">
+    <div class="flex items-center justify-between">
+      <span class="text-xl font-semibold">Your learnings</span>
 
-  <!-- Collection 2 -->
-  <Collection
-    title="Character and Citizenship Education"
-    icon={PersonStanding}
-    numberofpodcasts={8}
-    numberofnotes={3}
-    variant="rose"
-  />
+      <div class="flex gap-4">
+        <div class="flex h-14 w-12 items-center justify-center rounded-2xl bg-slate-100">
+          <div class="text-slate-950"><BrainCircuit class="h-6 w-6" /></div>
+        </div>
+        <div class="flex h-14 w-12 items-center justify-center rounded-2xl bg-slate-100">
+          <div class="text-slate-950"><Funnel class="h-6 w-6" /></div>
+        </div>
+      </div>
+    </div>
 
-  <!-- Collection 3 -->
-  <Collection
-    title="Inclusive Education"
-    icon={MessageCircleQuestion}
-    numberofpodcasts={10}
-    numberofnotes={1}
-    variant="amber"
-  />
+    <div class="grid auto-rows-fr grid-cols-2 gap-4 [&>:first-child]:col-span-2">
+      <!-- Collection 1 -->
+      <Collection
+        title="Special Educational Needs"
+        icon={HandHeart}
+        numberofpodcasts={12}
+        numberofnotes={2}
+        variant="purple"
+      />
+
+      <!-- Collection 2 -->
+      <Collection
+        title="Character and Citizenship Education"
+        icon={PersonStanding}
+        numberofpodcasts={8}
+        numberofnotes={3}
+        variant="rose"
+      />
+
+      <!-- Collection 3 -->
+      <Collection
+        title="Inclusive Education"
+        icon={MessageCircleQuestion}
+        numberofpodcasts={10}
+        numberofnotes={1}
+        variant="amber"
+      />
+    </div>
+  </div>
 </div>
