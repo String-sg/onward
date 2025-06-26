d3dfff12c87937fc38f04e01122b9ee88d9771d9
Kelly Lim <kellylim.06@gmail.com>
2025-06-27 00:26:46 +0800

    feat(learner): add `Collection` page with hardcoded values

--- apps/learner/src/routes/(app)/learning/collection/[id]/+page.svelte
+++ apps/learner/src/routes/(app)/learning/collection/[id]/+page.svelte
@@ -1,1 +1,21 @@
-<div class="px-6">Collection page!</div>
+<script>
+  import { ChevronLeft } from '@lucide/svelte';
+
+  import MultiLearningUnitMini from '$lib/components/MultiLearningUnitMini.svelte';
+</script>
+
+<div class="flex flex-col px-6">
+  <div class="flex flex-col gap-6">
+    <div class="flex items-center gap-3">
+      <div class="flex h-14 w-12 items-center justify-center rounded-[100px] bg-slate-100">
+        <ChevronLeft class="h-6 w-6 text-slate-950" />
+      </div>
+      <span class="text-xl font-medium text-slate-950">Special Educational Needs</span>
+    </div>
+    <MultiLearningUnitMini
+      to="/mlu/1"
+      tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
+      title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
+    />
+  </div>
+</div>
