139137c16ee19a5101bdbafa2a619596fde7f388
Kelly Lim <kellylim.06@gmail.com>
2025-06-27 16:59:20 +0800

    feat(learner): remove `BrainCircuit` and `Funnel` icons

--- apps/learner/src/routes/(app)/learning/+page.svelte
+++ apps/learner/src/routes/(app)/learning/+page.svelte
@@ -1,23 +1,10 @@
 <script lang="ts">
-  import { BrainCircuit, Funnel } from '@lucide/svelte';
-
   import Collection from '$lib/components/Collection.svelte';
 </script>
 
 <div class="flex flex-col px-6">
   <div class="flex flex-col gap-6">
-    <div class="flex items-center justify-between">
-      <span class="text-xl font-semibold">Your learnings</span>
-
-      <div class="flex gap-4">
-        <div class="flex items-center justify-center rounded-2xl bg-slate-100 p-3">
-          <BrainCircuit />
-        </div>
-        <div class="flex items-center justify-center rounded-2xl bg-slate-100 p-3">
-          <Funnel />
-        </div>
-      </div>
-    </div>
+    <span class="text-xl font-semibold">Your learnings</span>
 
     <div class="grid auto-rows-fr grid-cols-2 gap-4 [&>:first-child]:col-span-2">
       <!-- Collection 1 -->
