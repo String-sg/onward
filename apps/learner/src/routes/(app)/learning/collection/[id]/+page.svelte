75bb2e963c4d60954f733066b65f9f325f9bd8b7
Kelly Lim <kellylim.06@gmail.com>
2025-06-28 10:09:39 +0800

    chore(learner): update space between div and identify x axis for gap

--- apps/learner/src/routes/(app)/learning/collection/[id]/+page.svelte
+++ apps/learner/src/routes/(app)/learning/collection/[id]/+page.svelte
@@ -4,31 +4,30 @@
   import MultiLearningUnitMini from '$lib/components/MultiLearningUnitMini.svelte';
 </script>
 
-<div class="flex flex-col px-6">
-  <div class="flex flex-col gap-6">
-    <div class="flex items-center gap-3">
-      <a
-        href="/learning"
-        class="flex items-center justify-center rounded-full bg-slate-100 px-2 py-3"
-      >
-        <ChevronLeft />
-      </a>
-      <span class="text-xl font-medium">Special Educational Needs</span>
-    </div>
-    <div class="flex flex-col gap-3">
-      <span class="text-xl font-medium">12 podcasts</span>
+<div class="flex flex-col gap-y-6 px-6">
+  <div class="flex items-center gap-x-3">
+    <a
+      href="/learning"
+      class="flex items-center justify-center rounded-full bg-slate-100 px-2 py-3"
+    >
+      <ChevronLeft />
+    </a>
+    <span class="text-xl font-medium">Special Educational Needs</span>
+  </div>
 
-      <MultiLearningUnitMini
-        to="/mlu/1"
-        tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
-        title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
-      />
+  <div class="flex flex-col gap-3">
+    <span class="text-xl font-medium">12 podcasts</span>
 
-      <MultiLearningUnitMini
-        to="/mlu/1"
-        tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
-        title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
-      />
-    </div>
+    <MultiLearningUnitMini
+      to="/mlu/1"
+      tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
+      title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
+    />
+
+    <MultiLearningUnitMini
+      to="/mlu/1"
+      tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
+      title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
+    />
   </div>
 </div>
