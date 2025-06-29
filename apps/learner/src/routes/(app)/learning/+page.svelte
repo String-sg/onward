ae9eaaf85b4573aa48a264c636618613f4f91b6d
kellylimmm <54774084+kellylimmm@users.noreply.github.com>
2025-06-29 23:52:35 +0800

    feat(learner): add `Collection` page and navigation (#57)

--- apps/learner/src/routes/(app)/learning/+page.svelte
+++ apps/learner/src/routes/(app)/learning/+page.svelte
@@ -1,34 +1,39 @@
 <script lang="ts">
-  import { HandHeart, MessageCircleQuestion, PersonStanding } from '@lucide/svelte';
-
   import Collection from '$lib/components/Collection.svelte';
 </script>
 
-<div class="grid auto-rows-fr grid-cols-2 gap-4 px-6 pt-5 [&>:first-child]:col-span-2">
-  <!-- Collection 1 -->
-  <Collection
-    title="Special Educational Needs"
-    icon={HandHeart}
-    numberofpodcasts={12}
-    numberofnotes={2}
-    variant="purple"
-  />
+<div class="flex flex-col gap-y-6 px-6">
+  <span class="text-xl font-semibold">Your learnings</span>
 
-  <!-- Collection 2 -->
-  <Collection
-    title="Character and Citizenship Education"
-    icon={PersonStanding}
-    numberofpodcasts={8}
-    numberofnotes={3}
-    variant="rose"
-  />
+  <div class="grid auto-rows-fr grid-cols-2 gap-4 [&>:first-child]:col-span-2">
+    <!-- Collection 1 -->
+    <Collection
+      title="SEN peer support"
+      tag="Special Educational Needs"
+      numberofpodcasts={12}
+      numberofnotes={2}
+      variant="purple"
+      to="/learning/collection/1"
+    />
 
-  <!-- Collection 3 -->
-  <Collection
-    title="Inclusive Education"
-    icon={MessageCircleQuestion}
-    numberofpodcasts={10}
-    numberofnotes={1}
-    variant="amber"
-  />
+    <!-- Collection 2 -->
+    <Collection
+      title="Learn to use AI"
+      tag="Artificial Intelligence"
+      numberofpodcasts={8}
+      numberofnotes={3}
+      variant="amber"
+      to="/learning/collection/2"
+    />
+
+    <!-- Collection 3 -->
+    <Collection
+      title="Strategies: Basic counselling"
+      tag="Supporting Student socio-emotional learning"
+      numberofpodcasts={10}
+      numberofnotes={1}
+      variant="teal"
+      to="/learning/collection/3"
+    />
+  </div>
 </div>
--- apps/learner/src/routes/(app)/learning/collection/[id]/+page.svelte
+++ apps/learner/src/routes/(app)/learning/collection/[id]/+page.svelte
@@ -1,1 +1,33 @@
-<div class="px-6">Collection page!</div>
+<script>
+  import { ChevronLeft } from '@lucide/svelte';
+
+  import MultiLearningUnitMini from '$lib/components/MultiLearningUnitMini.svelte';
+</script>
+
+<div class="flex flex-col gap-y-6 px-6">
+  <div class="flex items-center gap-x-3">
+    <a
+      href="/learning"
+      class="flex items-center justify-center rounded-full bg-slate-100 px-2 py-3"
+    >
+      <ChevronLeft />
+    </a>
+    <span class="text-xl font-medium">Special Educational Needs</span>
+  </div>
+
+  <div class="flex flex-col gap-3">
+    <span class="text-xl font-medium">12 podcasts</span>
+
+    <MultiLearningUnitMini
+      to="/mlu/1"
+      tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
+      title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
+    />
+
+    <MultiLearningUnitMini
+      to="/mlu/1"
+      tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
+      title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
+    />
+  </div>
+</div>
