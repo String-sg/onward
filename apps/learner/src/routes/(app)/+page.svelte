6cb4a100b3d2ec0b65c3c109d7022e6a9014f06c
Kelly Lim <kellylim.06@gmail.com>
2025-06-30 13:54:36 +0800

    feat(learner): update mlu route to content

--- apps/learner/src/routes/(app)/+page.svelte
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="px-6">
   <MultiLearningUnitFull
-    to="/mlu/1"
+    to="/content/1"
     tags={[{ variant: 'purple', content: 'Special Educational Needs' }]}
     title="Navigating Special Educational Needs in Singapore: A Path to Inclusion"
   />
---
+++ apps/learner/src/routes/content/[id]/+page.svelte
@@ -0,0 +1,1 @@
+<div class="px-6 py-5">Learning unit content page!</div>
