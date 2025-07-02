31d414b274f7f812399e9bca2496c06e960af162
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-07-02 11:25:45 +0800

    feat(learner): quiz page for learner

---
+++ apps/learner/src/routes/content/[id]/quiz/+page.server.ts
@@ -0,0 +1,23 @@
+export const load = async () => {
+  return {
+    title:
+      'The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog',
+    questionAnswers: [
+      {
+        id: 123,
+        question: 'The quick brown fox jumps over the lazy dog ',
+        options: ['The quick', 'brown fox jumps', 'over the', 'lazy dog'],
+        answer: 2,
+        explanation: 'This is an explanation.',
+      },
+      {
+        id: 234,
+        question:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor',
+        options: ['Lorem', 'ipsum', 'dolor', 'sit amet'],
+        answer: 2,
+        explanation: 'This is an explanation.',
+      },
+    ],
+  };
+};
--- apps/learner/src/routes/content/[id]/quiz/+page.svelte
+++ apps/learner/src/routes/content/[id]/quiz/+page.svelte
@@ -1,1 +1,83 @@
-<div class="px-6 py-5">Quiz page!</div>
+<script lang="ts">
+  import { ArrowLeft } from '@lucide/svelte';
+
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
+
+  const { data } = $props();
+
+  let selectedOptionIndex = $state(-1);
+  let currentQuestionIndex = $state(0);
+
+  let currentQuestion = $derived(data.questionAnswers[currentQuestionIndex]);
+
+  const contentId = $derived(page.params.id);
+
+  function selectOption(index: number) {
+    selectedOptionIndex = index;
+  }
+
+  function getOptionLetter(index: number) {
+    return String.fromCharCode(65 + index);
+  }
+
+  function nextQuestion() {
+    if (selectedOptionIndex !== -1) {
+      if (currentQuestionIndex < data.questionAnswers.length - 1) {
+        currentQuestionIndex++;
+        selectedOptionIndex = -1;
+      } else {
+        //TODO: Redirect quiz to completion page
+        console.log('Quiz completed!');
+      }
+    }
+  }
+
+  function goBack() {
+    goto(`/content/${contentId}`);
+  }
+</script>
+
+<main class="mx-auto flex h-full w-full max-w-5xl flex-col bg-slate-100">
+  <div class="align-center flex justify-between px-6 py-5">
+    <button
+      class="align-center flex cursor-pointer gap-1 rounded-full bg-slate-200 p-2"
+      onclick={() => goBack()}
+    >
+      <ArrowLeft />
+    </button>
+  </div>
+  <div class="px-6 text-xl font-medium">{currentQuestion.question}</div>
+  <div class="flex flex-col items-start gap-2 px-6 pt-6">
+    {#each currentQuestion.options as option, index (option)}
+      <button
+        class="flex h-16 cursor-pointer items-center gap-3 self-stretch rounded-2xl bg-white p-3 {selectedOptionIndex ===
+        index
+          ? 'border border-black'
+          : ''}"
+        onclick={() => selectOption(index)}
+      >
+        <div
+          class="flex items-center justify-center gap-1 rounded-lg {selectedOptionIndex === index
+            ? 'bg-slate-900 text-white'
+            : 'bg-slate-100'} px-2 py-1"
+        >
+          {getOptionLetter(index)}
+        </div>
+        {option}
+      </button>
+    {/each}
+  </div>
+
+  <div class="py-9.5 mt-auto flex flex-col px-6">
+    <button
+      class="flex justify-center gap-1 rounded-full {selectedOptionIndex === -1
+        ? ' cursor-not-allowed bg-slate-900/[0.5]'
+        : 'cursor-pointer bg-slate-900'} px-1 py-4 text-white"
+      onclick={nextQuestion}
+      disabled={selectedOptionIndex === -1}
+    >
+      Check Answer
+    </button>
+  </div>
+</main>
