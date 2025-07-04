a30e029cdecc175e9472bd687fa55153730ae67c
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-07-04 14:37:53 +0800

    feat(learner): update styling to use style directive

--- apps/learner/src/lib/components/Progress.svelte
+++ apps/learner/src/lib/components/Progress.svelte
@@ -16,6 +16,6 @@
 <div {...otherProps} class="relative h-3 w-full overflow-hidden rounded-full bg-white">
   <div
     class="h-full w-full flex-1 bg-slate-900 transition-all"
-    style="transform: translateX(-{100 - value}%)"
+    style:transform={`translateX(-${100 - value}%)`}
   ></div>
 </div>
