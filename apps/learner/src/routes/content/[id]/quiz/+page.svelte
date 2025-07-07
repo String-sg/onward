48db7d40a9ed126ccc1f1af2b08f717ae6a1c828
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-07-07 09:56:47 +0800

    feat(learner): modal for quiz feedback

--- apps/learner/src/routes/content/[id]/quiz/+page.svelte
+++ apps/learner/src/routes/content/[id]/quiz/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { ArrowLeft } from '@lucide/svelte';
+  import { ArrowLeft, X } from '@lucide/svelte';
 
   import { page } from '$app/state';
   import Button from '$lib/components/Button.svelte';
@@ -16,6 +16,10 @@
     ((currentQuestionIndex + 1) / data.questionAnswers.length) * 100,
   );
 
+  let isFeedbackModalOpen = $state(false);
+
+  let isCorrectAnswer = $state(false);
+
   const contentId = $derived(page.params.id);
 
   function selectOption(index: number) {
@@ -31,13 +35,45 @@
       if (currentQuestionIndex < data.questionAnswers.length - 1) {
         currentQuestionIndex++;
         selectedOptionIndex = -1;
+        isFeedbackModalOpen = false;
       } else {
         //TODO: Redirect quiz to completion page
         console.log('Quiz completed!');
       }
     }
   }
+
+  function checkAnswer() {
+    isCorrectAnswer = selectedOptionIndex + 1 === currentQuestion.answer;
+  }
+
+  function showFeedbackModal() {
+    isFeedbackModalOpen = true;
+  }
 </script>
+
+{#snippet modalFeedbackButton(optionIndex: number)}
+  <div class="flex flex-col items-start">
+    <button
+      class={[
+        'gap-x-2.75 flex w-full items-center rounded-2xl bg-lime-200 p-3',
+        optionIndex !== currentQuestion.answer - 1 && '!bg-slate-100',
+      ]}
+    >
+      <span
+        class={[
+          'flex items-center justify-center rounded-lg bg-lime-400 px-2 py-1 text-xs font-semibold text-black',
+          !isCorrectAnswer && '!bg-slate-900 !text-white',
+        ]}
+      >
+        {getOptionLetter(optionIndex)}
+      </span>
+      <span class="text-left text-base">
+        {currentQuestion.options[optionIndex]}
+      </span>
+    </button>
+  </div>
+{/snippet}
 
 <div class="flex h-full flex-col gap-y-6 p-6">
   <div class="flex items-center gap-x-8">
@@ -75,5 +111,54 @@
     </div>
   </div>
 
-  <Button onclick={nextQuestion} disabled={selectedOptionIndex === -1}>Check Answer</Button>
+  <Button
+    onclick={() => {
+      checkAnswer();
+      showFeedbackModal();
+    }}
+    disabled={selectedOptionIndex === -1 || isFeedbackModalOpen}>Check Answer</Button
+  >
+
+  <div
+    class="fixed inset-0 flex items-end justify-center {isFeedbackModalOpen
+      ? 'visible'
+      : 'invisible'}"
+  >
+    <div
+      class="flex w-full max-w-5xl transform flex-col gap-y-5 rounded-t-3xl bg-white p-5 shadow-lg transition-all {isFeedbackModalOpen
+        ? 'translate-y-0 opacity-100'
+        : 'translate-y-full opacity-0'}"
+    >
+      <div class="flex items-center justify-between">
+        <span class="py-2.5 text-xl/7 font-medium"
+          >{isCorrectAnswer ? 'Correct answer!' : 'Not quite right!'}</span
+        >
+        <button
+          class="cursor-pointer rounded-full bg-slate-200 px-2.5 py-3"
+          onclick={() => (isFeedbackModalOpen = false)}
+        >
+          <X />
+        </button>
+      </div>
+
+      <div class="flex flex-col gap-y-6">
+        <div class="flex flex-col gap-y-2">
+          <span class="text-base font-medium">Your answer</span>
+          {@render modalFeedbackButton(selectedOptionIndex)}
+        </div>
+        {#if !isCorrectAnswer}
+          <div class="flex flex-col gap-y-2">
+            <span class="text-base font-medium">Correct answer</span>
+            {@render modalFeedbackButton(currentQuestion.answer - 1)}
+          </div>
+        {/if}
+        <div class="flex flex-col gap-1 rounded-2xl bg-slate-100 p-3">
+          <span class="text-base font-medium text-slate-500">Explanation</span>
+          <span class="text-base">{currentQuestion.explanation}</span>
+        </div>
+      </div>
+
+      <Button onclick={nextQuestion}>Continue</Button>
+    </div>
+  </div>
 </div>
