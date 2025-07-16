33aecad9bcdea1e332e0824e33a2076c35749459
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-07-16 17:41:37 +0800

    feat(learner): increase z-index of feedback modal

--- apps/learner/src/routes/content/[id]/quiz/+page.svelte
+++ apps/learner/src/routes/content/[id]/quiz/+page.svelte
@@ -166,7 +166,7 @@
 </div>
 
 {#if isFeedbackModalOpen}
-  <div class="fixed inset-0 flex items-end justify-center">
+  <div class="z-100 fixed inset-0 flex items-end justify-center">
     <div
       class="inset-shadow-sm flex max-h-[70vh] w-full max-w-5xl transform flex-col gap-y-5 rounded-t-3xl bg-white px-4 pb-3 pt-4 shadow-lg transition-all"
       transition:slide={{ axis: 'y' }}
