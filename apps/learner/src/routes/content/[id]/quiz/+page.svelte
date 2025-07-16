12a051b41f79d78b88ea05fc2cc1c1a4097e0e5d
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-07-16 16:16:22 +0800

    feat(learner): update quiz layout

--- apps/learner/src/routes/content/[id]/quiz/+page.svelte
+++ apps/learner/src/routes/content/[id]/quiz/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { ArrowLeft, X } from '@lucide/svelte';
+  import { onMount } from 'svelte';
   import { slide } from 'svelte/transition';
 
   import { page } from '$app/state';
@@ -52,6 +53,26 @@
   function closeFeedbackModal() {
     isFeedbackModalOpen = false;
   }
+
+  let isWithinViewport = $state(false);
+
+  let target: HTMLElement | null;
+
+  onMount(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      isWithinViewport = entry.isIntersecting;
+    });
+
+    if (target) {
+      observer.observe(target);
+    }
+
+    return () => {
+      if (target) {
+        observer.unobserve(target);
+      }
+    };
+  });
 </script>
 
 {#snippet modalFeedbackButton(optionIndex: number)}
@@ -77,83 +98,104 @@
   </div>
 {/snippet}
 
-<div class="flex h-full flex-col gap-y-6 p-6">
-  <div class="flex items-center gap-x-8">
-    <a class="rounded-full bg-slate-200 px-3 py-4" href="/content/{contentId}">
-      <ArrowLeft />
-    </a>
-    <Progress value={percentageCompleted} />
-    <span class="text-sm">{currentQuestionIndex + 1}/{data.questionAnswers.length}</span>
-  </div>
+<header class="fixed inset-x-0 top-0 z-50 bg-slate-100/90 backdrop-blur-sm">
+  <div
+    class={[
+      'absolute inset-x-0 top-full h-px bg-transparent transition-colors duration-300',
+      !isWithinViewport && '!bg-slate-950/7.5',
+    ]}
+  ></div>
 
-  <div class="flex flex-1 flex-col gap-y-6">
-    <span class="text-xl font-medium">{currentQuestion.question}</span>
-    <div class="flex flex-col items-start gap-y-2">
-      {#each currentQuestion.options as option, index (option)}
-        <button
-          class={[
-            'py-4.75 px-2.75 flex w-full cursor-pointer items-center gap-x-3 rounded-2xl border border-transparent bg-white',
-            selectedOptionIndex === index && '!border-black',
-          ]}
-          onclick={() => selectOption(index)}
+  <div class="mx-auto w-full max-w-5xl px-4 py-3">
+    <div class="flex items-center justify-between gap-x-8">
+      <div class="flex w-full items-center gap-x-3">
+        <a
+          href="/content/{contentId}"
+          class="rounded-full p-4 transition-colors hover:bg-slate-200"
         >
-          <span
-            class={[
-              'rounded-lg bg-slate-100 px-2 py-1 text-xs font-semibold',
-              selectedOptionIndex === index && '!bg-slate-900 !text-white',
-            ]}
-          >
-            {getOptionLetter(index)}
-          </span>
-          <span class="text-left">
-            {option}
-          </span>
-        </button>
-      {/each}
-    </div>
-  </div>
-
-  <Button onclick={handleCheckAnswer} disabled={selectedOptionIndex === -1 || isFeedbackModalOpen}>
-    Check Answer
-  </Button>
-
-  {#if isFeedbackModalOpen}
-    <div class="fixed inset-0 flex items-end justify-center">
-      <div
-        class="inset-shadow-sm flex max-h-[70vh] w-full max-w-5xl transform flex-col gap-y-5 rounded-t-3xl bg-white p-5 shadow-lg transition-all"
-        transition:slide={{ axis: 'y' }}
-      >
-        <div class="flex items-center justify-between">
-          <span class="py-2.5 text-xl font-medium">
-            {isCorrectAnswer ? 'Correct answer!' : 'Not quite right!'}
-          </span>
-          <button
-            class="cursor-pointer rounded-full bg-slate-100 px-2.5 py-3 hover:bg-slate-200"
-            onclick={closeFeedbackModal}
-          >
-            <X />
-          </button>
-        </div>
-
-        <div class="flex flex-col gap-y-6 overflow-y-auto">
-          <div class="flex flex-col gap-y-2">
-            <span class="font-medium">Your answer</span>
-            {@render modalFeedbackButton(selectedOptionIndex)}
-          </div>
-          {#if !isCorrectAnswer}
-            <div class="flex flex-col gap-y-2">
-              <span class="font-medium">Correct answer</span>
-              {@render modalFeedbackButton(currentQuestion.answer - 1)}
-            </div>
-          {/if}
-          <div class="flex flex-col gap-y-1 rounded-2xl bg-slate-100 p-3">
-            <span class="font-medium text-zinc-600">Explanation</span>
-            <span>{currentQuestion.explanation}</span>
-          </div>
-        </div>
-
-        <Button onclick={nextQuestion}>Continue</Button>
+          <ArrowLeft />
+        </a>
+        <Progress value={percentageCompleted} />
+        <span class="text-sm">{currentQuestionIndex + 1}/{data.questionAnswers.length}</span>
       </div>
     </div>
-  {/if}
-</div>
+  </div>
+</header>
+
+<main class="pt-23 relative mx-auto min-h-full w-full max-w-5xl px-4 pb-3">
+  <div bind:this={target} class="absolute inset-x-0 top-0 h-px"></div>
+  <div class="flex h-full flex-col">
+    <div class="flex flex-1 flex-col gap-y-6">
+      <span class="text-xl font-medium">{currentQuestion.question}</span>
+      <div class="flex flex-col items-start gap-y-2">
+        {#each currentQuestion.options as option, index (option)}
+          <button
+            class={[
+              'py-4.75 px-2.75 flex w-full cursor-pointer items-center gap-x-3 rounded-2xl border border-transparent bg-white',
+              selectedOptionIndex === index && '!border-black',
+            ]}
+            onclick={() => selectOption(index)}
+          >
+            <span
+              class={[
+                'rounded-lg bg-slate-100 px-2 py-1 text-xs font-semibold',
+                selectedOptionIndex === index && '!bg-slate-900 !text-white',
+              ]}
+            >
+              {getOptionLetter(index)}
+            </span>
+            <span class="text-left">
+              {option}
+            </span>
+          </button>
+        {/each}
+      </div>
+    </div>
+    <Button
+      onclick={handleCheckAnswer}
+      disabled={selectedOptionIndex === -1 || isFeedbackModalOpen}
+    >
+      Check Answer
+    </Button>
+  </div>
+</main>
+
+{#if isFeedbackModalOpen}
+  <div class="fixed inset-0 flex items-end justify-center">
+    <div
+      class="inset-shadow-sm flex max-h-[70vh] w-full max-w-5xl transform flex-col gap-y-5 rounded-t-3xl bg-white px-4 pb-3 pt-4 shadow-lg transition-all"
+      transition:slide={{ axis: 'y' }}
+    >
+      <div class="flex items-center justify-between">
+        <span class="py-2.5 text-xl font-medium">
+          {isCorrectAnswer ? 'Correct answer!' : 'Not quite right!'}
+        </span>
+        <button
+          class="cursor-pointer rounded-full bg-slate-100 px-2.5 py-3 hover:bg-slate-200"
+          onclick={closeFeedbackModal}
+        >
+          <X />
+        </button>
+      </div>
+
+      <div class="flex flex-col gap-y-6 overflow-y-auto">
+        <div class="flex flex-col gap-y-2">
+          <span class="font-medium">Your answer</span>
+          {@render modalFeedbackButton(selectedOptionIndex)}
+        </div>
+        {#if !isCorrectAnswer}
+          <div class="flex flex-col gap-y-2">
+            <span class="font-medium">Correct answer</span>
+            {@render modalFeedbackButton(currentQuestion.answer - 1)}
+          </div>
+        {/if}
+        <div class="flex flex-col gap-y-1 rounded-2xl bg-slate-100 p-3">
+          <span class="font-medium text-zinc-600">Explanation</span>
+          <span>{currentQuestion.explanation}</span>
+        </div>
+      </div>
+
+      <Button onclick={nextQuestion}>Continue</Button>
+    </div>
+  </div>
+{/if}
