711eb7ef72b449229079cbf84c6db010a828be6e
Kelly Lim <kellylim.06@gmail.com>
2025-07-16 17:54:33 +0800

    feat(learner): remove absolute and relative header layout

--- apps/learner/src/routes/podcast/[id]/+page.svelte
+++ apps/learner/src/routes/podcast/[id]/+page.svelte
@@ -3,8 +3,6 @@
 </script>
 
 <header class="fixed inset-x-0 top-0 z-50 bg-slate-100/90 backdrop-blur-sm">
-  <div class="absolute inset-x-0 top-full h-px bg-transparent transition-colors duration-300"></div>
-
   <div class="mx-auto w-full max-w-5xl px-4 py-3">
     <div class="flex items-center justify-between gap-x-8">
       <a href="/" class="rounded-full p-4 transition-colors hover:bg-slate-200">
@@ -18,6 +16,6 @@
   </div>
 </header>
 
-<main class="pt-23 relative mx-auto min-h-full w-full max-w-5xl px-4 pb-3">
+<main class="pt-23 mx-auto min-h-full w-full max-w-5xl px-4 pb-3">
   <!-- Main content of the page -->
 </main>
