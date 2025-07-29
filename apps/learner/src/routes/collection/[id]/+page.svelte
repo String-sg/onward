13152c6b859beb7be617a169540afc8657d39954
Yiming <yimingpeh@gmail.com>
2025-07-29 15:06:30 +0900

    feat(learner): add focus effect to navigation buttons and adjust their padding from `px-3 py-4` to `p-4` on `Collection` and `LearningUnit` pages (#140)

--- apps/learner/src/routes/collection/[id]/+page.svelte
+++ apps/learner/src/routes/collection/[id]/+page.svelte
@@ -20,7 +20,10 @@
   <div class="mx-auto w-full max-w-5xl px-4 py-3">
     <div class="flex items-center justify-between gap-x-8">
       <div class="flex items-center gap-x-3">
-        <a href="/learning" class="rounded-full px-3 py-4 transition-colors hover:bg-slate-200">
+        <a
+          href="/learning"
+          class="rounded-full p-4 transition-colors hover:bg-slate-200 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-950"
+        >
           <ArrowLeft />
         </a>
 
