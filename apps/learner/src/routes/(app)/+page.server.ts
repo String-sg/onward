284604d01de8b9f99dd8f71502718ce1e38c3b96
Kelly Lim <kellylim.06@gmail.com>
2025-07-31 15:46:33 +0800

    chore(learner): remove createdAt in data

--- apps/learner/src/routes/(app)/+page.server.ts
+++ apps/learner/src/routes/(app)/+page.server.ts
@@ -5,6 +5,5 @@ export const load: PageServerLoad = async () => {
     id: 1,
     tags: ['Special Educational Needs'],
     title: 'Navigating Special Educational Needs: A Path to Inclusion',
-    createdAt: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000),
   };
 };
