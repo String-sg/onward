28da94e9e435f9a8453c62f5ed2e2ca3b895a198
Kelly Lim <kellylim.06@gmail.com>
2025-08-05 15:22:21 +0800

    chore(learner): remove fixed and add flex-1 for player content to stay at bottom

--- apps/learner/src/routes/(app)/+page.svelte
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -79,80 +79,80 @@
         </button>
       </header>
 
-      <div class="fixed inset-x-0 bottom-0">
-        <div class="mx-auto flex w-full max-w-5xl flex-col items-center px-6">
-          <!-- Badge and Title -->
-          <div class="flex w-full flex-col items-start gap-y-3">
-            <Badge variant="purple">Special Educational Needs</Badge>
-            <div class="mb-6 w-full text-xl">
-              Navigating Special Educational Needs: A Path to Inclusion
-            </div>
+      <div class="flex-1"></div>
+
+      <div class="mx-auto flex w-full max-w-5xl flex-col items-center px-6">
+        <!-- Badge and Title -->
+        <div class="flex w-full flex-col items-start gap-y-3">
+          <Badge variant="purple">Special Educational Needs</Badge>
+          <div class="mb-6 w-full text-xl">
+            Navigating Special Educational Needs: A Path to Inclusion
           </div>
+        </div>
 
-          <!-- Slider -->
-          <div class="group relative mb-2 h-2 w-full rounded-full bg-slate-700">
-            <!-- Hardcoded: Filled (Progress) -->
-            <div class="h-full w-3/4 rounded-full bg-white"></div>
+        <!-- Slider -->
+        <div class="group relative mb-2 h-2 w-full rounded-full bg-slate-700">
+          <!-- Hardcoded: Filled (Progress) -->
+          <div class="h-full w-3/4 rounded-full bg-white"></div>
 
-            <!-- Thumb -->
-            <div
-              class="absolute left-3/4 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full bg-white opacity-0 shadow transition-opacity group-hover:opacity-100"
-              draggable="true"
-            ></div>
-          </div>
+          <!-- Thumb -->
+          <div
+            class="absolute left-3/4 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full bg-white opacity-0 shadow transition-opacity group-hover:opacity-100"
+            draggable="true"
+          ></div>
+        </div>
 
-          <!-- Time Display -->
-          <div class="flex w-full justify-between">
-            <span>14:32</span>
-            <span>-2.00</span>
-          </div>
+        <!-- Time Display -->
+        <div class="flex w-full justify-between">
+          <span>14:32</span>
+          <span>-2.00</span>
+        </div>
 
-          <!-- Speed Control -->
-          <div class="flex w-full justify-center py-5">
-            <button
-              class="flex cursor-pointer items-center rounded-full bg-white/20 px-4 py-2 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              <span class="text-sm font-medium">1.0x speed</span>
-            </button>
-          </div>
+        <!-- Speed Control -->
+        <div class="flex w-full justify-center py-5">
+          <button
+            class="flex cursor-pointer items-center rounded-full bg-white/20 px-4 py-2 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            <span class="text-sm font-medium">1.0x speed</span>
+          </button>
+        </div>
 
-          <!-- Playback Controls -->
-          <div class="flex w-full justify-evenly py-4">
-            <!-- Backward Button -->
-            <button
-              class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              <SkipBack />
-            </button>
+        <!-- Playback Controls -->
+        <div class="flex w-full justify-evenly py-4">
+          <!-- Backward Button -->
+          <button
+            class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            <SkipBack />
+          </button>
 
-            <!-- Replay Button -->
-            <button
-              class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              <RotateCcw />
-            </button>
+          <!-- Replay Button -->
+          <button
+            class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            <RotateCcw />
+          </button>
 
-            <!-- Play/Pause Button -->
-            <button
-              class="cursor-pointer rounded-full bg-white p-4 text-black transition-colors hover:bg-white/50 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              <Pause />
-            </button>
+          <!-- Play/Pause Button -->
+          <button
+            class="cursor-pointer rounded-full bg-white p-4 text-black transition-colors hover:bg-white/50 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            <Pause />
+          </button>
 
-            <!-- Forward Button -->
-            <button
-              class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              <RotateCw />
-            </button>
+          <!-- Forward Button -->
+          <button
+            class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            <RotateCw />
+          </button>
 
-            <!-- Next Button -->
-            <button
-              class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              <SkipForward />
-            </button>
-          </div>
+          <!-- Next Button -->
+          <button
+            class="cursor-pointer rounded-full p-4 transition-colors hover:bg-white/20 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            <SkipForward />
+          </button>
         </div>
       </div>
     </div>
