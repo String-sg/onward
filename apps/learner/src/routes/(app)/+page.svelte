d17adea67a68f5d4955d34d15e083ef534eab7c9
santosral <sralphian029@gmail.com>
2025-08-05 14:51:31 +0800

    feat(learner): move floating player to root layout to make it persistent

--- apps/learner/src/routes/(app)/+page.svelte
+++ apps/learner/src/routes/(app)/+page.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-  import { ArrowLeft } from '@lucide/svelte';
-
-  import FloatingChat from '$lib/components/FloatingChat.svelte';
-  import FloatingPlayer from '$lib/components/FloatingPlayer.svelte';
   import LearningUnit from '$lib/components/LearningUnit.svelte';
   import { AudioState } from '$lib/helpers/index.js';
-
-  let isModalOpen = $state(false);
 
   const audioState = AudioState.load();
 
@@ -14,23 +8,11 @@
     audioState.isPlaying ? audioState.currentLearningUnit?.id : null,
   );
 
-  const handleFloatingPlayerClick = () => {
-    isModalOpen = !isModalOpen;
-  };
-
   const handlePlay = (learningUnit: { id: number; title: string }) => {
     if (currentPlayingId === learningUnit.id) {
       audioState.pause();
     } else {
       audioState.play(learningUnit);
-    }
-  };
-
-  const handleResume = () => {
-    if (audioState.isPlaying) {
-      audioState.pause();
-    } else {
-      audioState.resume();
     }
   };
 </script>
@@ -68,38 +50,3 @@
     />
   </div>
 </div>
-
-<div class="z-100 pointer-events-none fixed inset-x-0 bottom-0">
-  <div class="mx-auto max-w-5xl px-4 py-3">
-    <div class="flex justify-end gap-x-4">
-      {#if audioState.isFloatingPlayerVisible}
-        <div class="pointer-events-auto flex-grow overflow-x-hidden py-3">
-          <FloatingPlayer
-            title={audioState.currentLearningUnit?.title}
-            isplaying={audioState.isPlaying}
-            onplay={handleResume}
-            onclick={handleFloatingPlayerClick}
-          />
-        </div>
-      {/if}
-
-      <FloatingChat />
-    </div>
-  </div>
-</div>
-
-{#if isModalOpen}
-  <div class="z-200 fixed inset-0 bg-slate-950">
-    <div class="mx-auto w-full max-w-5xl px-4 py-3">
-      <!-- Modal Header -->
-      <header class="flex items-center">
-        <button
-          class="rounded-full p-4 transition-colors hover:bg-white/20"
-          onclick={handleFloatingPlayerClick}
-        >
-          <ArrowLeft class="text-white" />
-        </button>
-      </header>
-    </div>
-  </div>
-{/if}
