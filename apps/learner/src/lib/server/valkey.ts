180b7c67a27c9413e2c29d785d0c2ae84b757bba
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-08-26 13:59:05 +0800

    feat(learner): prevent valkey from initializing during runtime

--- apps/learner/src/lib/server/valkey.ts
+++ apps/learner/src/lib/server/valkey.ts
@@ -2,26 +2,29 @@ import process from 'node:process';
 
 import { GlideClient } from '@valkey/valkey-glide';
 
+import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 
 const url = new URL(env.VALKEY_URL || 'valkey://default:secret@localhost:6379/0');
 
-export const valkey = await GlideClient.createClient({
-  addresses: [
-    {
-      host: url.hostname,
-      port: Number(url.port) || 6379,
-    },
-  ],
-  credentials: {
-    username: url.username,
-    password: url.password,
-  },
-  databaseId: Number(url.pathname.slice(1)) || 0,
+export const valkey = building
+  ? ({} as GlideClient)
+  : await GlideClient.createClient({
+      addresses: [
+        {
+          host: url.hostname,
+          port: Number(url.port) || 6379,
+        },
+      ],
+      credentials: {
+        username: url.username,
+        password: url.password,
+      },
+      databaseId: Number(url.pathname.slice(1)) || 0,
 
-  // Enable TLS if the query string contains `tls=true`.
-  useTLS: url.searchParams.get('tls') === 'true',
-});
+      // Enable TLS if the query string contains `tls=true`.
+      useTLS: url.searchParams.get('tls') === 'true',
+    });
 
 // Close the connection to the Valkey instance when the server shuts down.
 process.on('sveltekit:shutdown', () => {
