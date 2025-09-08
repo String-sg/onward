ef90a8d12260ee398cbfbe07baeba5bd2061aa7c
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-09-08 16:54:09 +0800

    chore(learner): script for seeding data

---
+++ apps/learner/prisma/seed.ts
@@ -0,0 +1,132 @@
+import * as dotenv from 'dotenv';
+
+import { type Prisma, PrismaClient } from '../src/generated';
+
+dotenv.config({ path: '../.env' });
+
+enum ContentType {
+  Podcast = 'podcast',
+}
+
+const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.POSTGRES_URL || 'postgresql://root:secret@localhost:5432/onward-dev',
+    },
+  },
+});
+
+const collections = [
+  {
+    id: 1,
+    data: {
+      title: 'SEN Peer Support',
+      tag: 'Special Educational Needs',
+    },
+  },
+  {
+    id: 2,
+    data: {
+      title: 'Learn to use AI',
+      tag: 'Artificial Intelligence',
+    },
+  },
+  {
+    id: 3,
+    data: {
+      title: 'Understanding Mental Health',
+      tag: 'Teacher mental health literacy',
+    },
+  },
+];
+
+//TODO: update content URL after seedbox is up
+const learningUnits = [
+  {
+    id: 1,
+    data: {
+      title: 'SEN Learning Unit 1',
+      tags: ['Special Education Needs', 'Podcast'],
+      contentType: ContentType.Podcast,
+      contentURL: 'https://example.com',
+      collectionId: 1,
+      summary: 'This is a summary',
+    },
+  },
+  {
+    id: 2,
+    data: {
+      title: 'SEN Learning Unit 2',
+      tags: ['Special Education Needs', 'Podcast'],
+      contentType: ContentType.Podcast,
+      contentURL: 'https://example.com',
+      collectionId: 1,
+      summary: 'This is a summary',
+    },
+  },
+  {
+    id: 3,
+    data: {
+      title: 'AI Learning Unit 1',
+      tags: ['Artificial Intelligence', 'Podcast'],
+      contentType: ContentType.Podcast,
+      contentURL: 'https://example.com',
+      collectionId: 2,
+      summary: 'This is a summary',
+    },
+  },
+];
+
+async function main() {
+  await createUser();
+
+  for (const collection of collections) {
+    await createCollection(collection);
+  }
+
+  for (const learningUnit of learningUnits) {
+    await createLearningUnit(learningUnit);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
+
+async function createUser() {
+  return db.user.upsert({
+    where: {
+      id: 1,
+    },
+    update: {},
+    create: {
+      email: 'test@example.com',
+      name: 'Test User',
+      googleProviderId: '1234567890',
+    },
+  });
+}
+
+async function createCollection(collection: { id: number; data: Prisma.CollectionCreateInput }) {
+  return db.collection.upsert({
+    where: { id: collection.id },
+    update: {},
+    create: collection.data,
+  });
+}
+
+async function createLearningUnit(learningUnit: {
+  id: number;
+  data: Prisma.LearningUnitUncheckedCreateInput;
+}) {
+  return db.learningUnit.upsert({
+    where: { id: learningUnit.id },
+    update: {},
+    create: learningUnit.data,
+  });
+}
