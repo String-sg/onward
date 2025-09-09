6e241461d05fcda1679d4c283c35e8cd7966b8f2
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-09-09 14:21:16 +0800

    feat(learner): update schema to remove user dependency on collection

---
+++ prisma/migrations/20250909062020_remove_user_association_from_collection/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `kind` on the `collections` table. All the data in the column will be lost.
+  - You are about to drop the column `user_id` on the `collections` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "collections" DROP CONSTRAINT "collections_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "collections" DROP COLUMN "kind",
+DROP COLUMN "user_id";
