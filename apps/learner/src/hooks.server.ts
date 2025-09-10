cc6c696b56363c731cab0d0fc3d9f975c4233c2b
YimingIsCOLD <yimingpeh@gmail.com>
2025-09-10 15:26:22 +0800

    refactor: split hooks to improve code organisation

--- apps/learner/src/hooks.server.ts
+++ apps/learner/src/hooks.server.ts
@@ -5,12 +5,29 @@ import { nanoid } from '$lib/helpers/index.js';
 import { auth } from '$lib/server/auth.js';
 import { logger } from '$lib/server/logger.js';
 
-export const handle: Handle = sequence(auth.handle, async ({ event, resolve }) => {
+/**
+ * A handle that adds a request ID for tracing and attaches a scoped logger to the event.
+ */
+const requestLoggingHandle: Handle = async ({ event, resolve }) => {
   const requestId = nanoid();
 
   event.locals.logger = logger.child({ requestId });
   event.setHeaders({ 'X-Request-Id': requestId });
 
+  return await resolve(event);
+};
+
+/**
+ * A handle that protects routes that requires authentication.
+ *
+ * For unauthenticated users:
+ * - Redirects root path `/` to `/login`
+ * - Redirects protected routes to `/login`
+ *
+ * For authenticated users:
+ * - Redirects `/login` to root path `/`
+ */
+const authenticationHandle: Handle = async ({ event, resolve }) => {
   if (!event.locals.session.isAuthenticated) {
     if (event.url.pathname === '/') {
       return redirect(303, '/login');
@@ -32,4 +49,6 @@ export const handle: Handle = sequence(auth.handle, async ({ event, resolve }) =
   }
 
   return await resolve(event);
-});
+};
+
+export const handle: Handle = sequence(auth.handle, requestLoggingHandle, authenticationHandle);
