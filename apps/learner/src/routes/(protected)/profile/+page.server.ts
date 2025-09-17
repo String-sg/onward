7ca68e37a00dd12ae7cd221ba2a8312c45090a22
kellylimmm <54774084+kellylimmm@users.noreply.github.com>
2025-09-17 17:34:20 +0800

    feat(learner): update profile content data with dynamic data (#224)

--- apps/learner/src/routes/(protected)/profile/+page.server.ts
+++ apps/learner/src/routes/(protected)/profile/+page.server.ts
@@ -1,12 +1,51 @@
+import { error, redirect } from '@sveltejs/kit';
+import { startOfMonth, startOfWeek } from 'date-fns';
+
+import { db } from '$lib/server/db';
+
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-  return {
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    learningUnitsConsumedByMonth: '8',
-    learningUnitsConsumedByWeek: '6',
-    learningUnitsCompletedByMonth: '4',
-    learningUnitsCompletedByWeek: '2',
-  };
+export const load: PageServerLoad = async (event) => {
+  const logger = event.locals.logger.child({ handler: 'user_profile' });
+
+  const { user } = event.locals.session;
+  if (!user) {
+    logger.warn('User is not authenticated');
+    return redirect(303, '/login');
+  }
+
+  const now = new Date();
+  const firstOfMonth = startOfMonth(now);
+  const startOfWeekMonday = startOfWeek(now, { weekStartsOn: 1 });
+
+  try {
+    const [byWeek, byMonth] = await Promise.all([
+      db.learningJourney.groupBy({
+        by: ['isCompleted'],
+        where: { userId: BigInt(user.id), updatedAt: { gte: startOfWeekMonday } },
+        _count: { _all: true },
+      }),
+      db.learningJourney.groupBy({
+        by: ['isCompleted'],
+        where: { userId: BigInt(user.id), updatedAt: { gte: firstOfMonth } },
+        _count: { _all: true },
+      }),
+    ]);
+
+    return {
+      name: user.name,
+      email: user.email,
+      learningUnitsConsumedByMonth: byMonth.reduce((total, group) => total + group._count._all, 0),
+      learningUnitsConsumedByWeek: byWeek.reduce((total, group) => total + group._count._all, 0),
+      learningUnitsCompletedByMonth: byMonth.find((group) => group.isCompleted)?._count._all ?? 0,
+      learningUnitsCompletedByWeek: byWeek.find((group) => group.isCompleted)?._count._all ?? 0,
+    };
+  } catch (err) {
+    logger.error(
+      { err, userId: user.id },
+      'Unknown error occurred while retrieving learning journey counts',
+    );
+
+    throw error(500);
+  }
 };
