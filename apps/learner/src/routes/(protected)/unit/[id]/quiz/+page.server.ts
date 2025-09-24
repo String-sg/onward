d637437b3cf37f80a479ed1e4af99a0a6f623216
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-09-24 16:40:14 +0800

    feat(learner): switch hardcoded quiz data with database queries and data

--- apps/learner/src/routes/(protected)/unit/[id]/quiz/+page.server.ts
+++ apps/learner/src/routes/(protected)/unit/[id]/quiz/+page.server.ts
@@ -1,31 +1,48 @@
+import { error, redirect } from '@sveltejs/kit';
+
+import { db, type QuestionAnswerFindManyArgs, type QuestionAnswerGetPayload } from '$lib/server/db';
+
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-  return {
-    questions: [
-      {
-        id: 123,
-        question: 'What is the color of the sky?',
-        options: ['Blue', 'Red', 'Green', 'Yellow'],
-        answerIndex: 0,
-        explanation: 'This is an explanation.',
-      },
-      {
-        id: 234,
-        question:
-          'This is a super long question that is going to wrap around to the next line and then some more text to see how it looks. It should be long enough to wrap around to the next line and then some more text to see how it looks. I want to see how it looks when it wraps around to the next line and then some more text to see how it looks.',
-        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-        answerIndex: 0,
-        explanation:
-          'This is an explanation. It is a very long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long explanation.',
-      },
-      {
-        id: 345,
-        question: 'How many days are in a week?',
-        options: ['7', '8', '9', '10'],
-        answerIndex: 0,
-        explanation: 'This is an explanation.',
-      },
-    ],
-  };
+export const load: PageServerLoad = async (event) => {
+  const logger = event.locals.logger.child({ handler: 'quiz' });
+
+  const { user } = event.locals.session;
+  if (!user) {
+    logger.warn('User not authenticated');
+    return redirect(303, '/login');
+  }
+
+  const questionAnswerArgs = {
+    select: {
+      id: true,
+      question: true,
+      options: true,
+      answer: true,
+      explanation: true,
+      order: true,
+    },
+    where: {
+      learningUnitId: BigInt(event.params.id),
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  } satisfies QuestionAnswerFindManyArgs;
+
+  let questionAnswers: QuestionAnswerGetPayload<typeof questionAnswerArgs>[];
+
+  try {
+    questionAnswers = await db.questionAnswer.findMany(questionAnswerArgs);
+  } catch (err) {
+    logger.error({ err }, 'Failed to retrieve quiz records');
+    throw error(500);
+  }
+
+  if (!questionAnswers.length) {
+    logger.error('No quiz records found');
+    throw error(404);
+  }
+
+  return { questionAnswers };
 };
