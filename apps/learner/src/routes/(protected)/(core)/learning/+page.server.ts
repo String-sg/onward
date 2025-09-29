99816b90d129901b7a0c6380996c928c163b106d
Kelly Lim <kellylim.06@gmail.com>
2025-09-29 12:22:25 +0800

    feat(learner): update inline statement for export and for raw sql

--- apps/learner/src/routes/(protected)/(core)/learning/+page.server.ts
+++ apps/learner/src/routes/(protected)/(core)/learning/+page.server.ts
@@ -1,7 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
-import type { CollectionModel, TagModel } from '$lib/server/db';
-import { db } from '$lib/server/db';
+import { type CollectionModel, db, type TagModel } from '$lib/server/db';
 
 import type { PageServerLoad } from './$types';
 
@@ -19,29 +18,30 @@ export const load: PageServerLoad = async (event) => {
     tags: Pick<TagModel, 'code' | 'label'>[];
   }
 
-  const collections = await db.$queryRaw<CollectionWithTags[]>`SELECT
-    c.id AS id,
-    c.title AS title,
-    c.type AS type,
-    COUNT(DISTINCT lj.id) AS "numberOfPodcasts",
-    COALESCE(
-      (
-        SELECT JSON_AGG(
-                JSONB_BUILD_OBJECT('code', t.code, 'label', t.label)
-                ORDER BY t.created_at
-              )
-        FROM collection_tags ct
-        INNER JOIN tags t ON t.id = ct.tag_id
-        WHERE ct.collection_id = c.id
-      ),
-      '[]'
-    ) AS tags
-  FROM learning_journeys lj
-  INNER JOIN learning_units lu ON lu.id = lj.learning_unit_id
-  INNER JOIN collections c ON c.id = lu.collection_id
-  WHERE lj.user_id = ${user.id}
-  GROUP BY c.id;
-`;
+  const collections = await db.$queryRaw<CollectionWithTags[]>`
+    SELECT
+      c.id AS id,
+      c.title AS title,
+      c.type AS type,
+      COUNT(DISTINCT lj.id) AS "numberOfPodcasts",
+      COALESCE(
+        (
+          SELECT JSON_AGG(
+                  JSONB_BUILD_OBJECT('code', t.code, 'label', t.label)
+                  ORDER BY t.created_at
+                )
+          FROM collection_tags ct
+          INNER JOIN tags t ON t.id = ct.tag_id
+          WHERE ct.collection_id = c.id
+        ),
+        '[]'
+      ) AS tags
+    FROM learning_journeys lj
+    INNER JOIN learning_units lu ON lu.id = lj.learning_unit_id
+    INNER JOIN collections c ON c.id = lu.collection_id
+    WHERE lj.user_id = ${user.id}
+    GROUP BY c.id;
+  `;
 
   return {
     collections,
