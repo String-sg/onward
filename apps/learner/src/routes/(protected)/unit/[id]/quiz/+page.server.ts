a9080491bf277e7870102e79ea8eca063030e319
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-09-29 15:13:05 +0800

    feat(learner): clean up server queries

--- apps/learner/src/routes/(protected)/unit/[id]/quiz/+page.server.ts
+++ apps/learner/src/routes/(protected)/unit/[id]/quiz/+page.server.ts
@@ -1,18 +1,11 @@
 import { error, redirect } from '@sveltejs/kit';
 
-import {
-  db,
-  type LearningUnitFindUniqueArgs,
-  type LearningUnitGetPayload,
-  type QuestionAnswerFindManyArgs,
-  type QuestionAnswerGetPayload,
-  type TagGetPayload,
-} from '$lib/server/db';
+import { db, type LearningUnitFindUniqueArgs, type LearningUnitGetPayload } from '$lib/server/db';
 
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-  const logger = event.locals.logger.child({ handler: 'quiz' });
+  const logger = event.locals.logger.child({ handler: 'page_quiz' });
 
   const { user } = event.locals.session;
   if (!user) {
@@ -20,42 +13,33 @@ export const load: PageServerLoad = async (event) => {
     return redirect(303, '/login');
   }
 
-  const questionAnswerArgs = {
+  const learningUnitArgs = {
     select: {
-      id: true,
-      question: true,
-      options: true,
-      answer: true,
-      explanation: true,
-      order: true,
-    },
-    where: {
-      learningUnitId: BigInt(event.params.id),
-    },
-    orderBy: {
-      order: 'asc',
-    },
-  } satisfies QuestionAnswerFindManyArgs;
-
-  let questionAnswers: QuestionAnswerGetPayload<typeof questionAnswerArgs>[];
-
-  try {
-    questionAnswers = await db.questionAnswer.findMany(questionAnswerArgs);
-  } catch (err) {
-    logger.error({ err }, 'Failed to retrieve quiz records');
-    throw error(500);
-  }
-
-  if (!questionAnswers.length) {
-    logger.error('No quiz records found');
-    throw error(404);
-  }
-
-  const learningUnitTypeArgs = {
-    select: {
+      questionAnswers: {
+        select: {
+          id: true,
+          question: true,
+          options: true,
+          answer: true,
+          explanation: true,
+          order: true,
+        },
+        orderBy: {
+          order: 'asc',
+        },
+      },
       collection: {
         select: {
           type: true,
+          tags: {
+            select: {
+              tag: {
+                select: {
+                  label: true,
+                },
+              },
+            },
+          },
         },
       },
     },
@@ -64,41 +48,27 @@ export const load: PageServerLoad = async (event) => {
     },
   } satisfies LearningUnitFindUniqueArgs;
 
-  let learningUnitType: LearningUnitGetPayload<typeof learningUnitTypeArgs> | null;
+  let learningUnit: LearningUnitGetPayload<typeof learningUnitArgs> | null;
   try {
-    learningUnitType = await db.learningUnit.findUnique(learningUnitTypeArgs);
+    learningUnit = await db.learningUnit.findUnique(learningUnitArgs);
   } catch (err) {
-    logger.error({ err }, 'Failed to retrieve learning unit with type');
+    logger.error({ err }, 'Failed to retrieve learning unit with quiz data');
     throw error(500);
   }
 
-  if (!learningUnitType) {
+  if (!learningUnit) {
     logger.error('Learning unit not found');
     throw error(404);
   }
 
-  const tagArgs = {
-    select: {
-      label: true,
-    },
-    where: {
-      code: learningUnitType.collection.type,
-    },
+  if (!learningUnit.questionAnswers.length) {
+    logger.warn('No quiz records found');
+    return redirect(303, `/unit/${event.params.id}`);
+  }
+
+  return {
+    questionAnswers: learningUnit.questionAnswers,
+    type: learningUnit.collection.type,
+    label: learningUnit.collection.tags[0]?.tag.label,
   };
-
-  let tagLabel: TagGetPayload<typeof tagArgs> | null;
-
-  try {
-    tagLabel = await db.tag.findUnique(tagArgs);
-  } catch (err) {
-    logger.error({ err }, 'Failed to retrieve tag label');
-    throw error(500);
-  }
-
-  if (!tagLabel) {
-    logger.error('Tag label not found');
-    throw error(404);
-  }
-
-  return { questionAnswers, type: learningUnitType.collection.type, label: tagLabel.label };
 };
