3c9b5cf980538107f99873607285ed2e207b7efc
kellylimmm <54774084+kellylimmm@users.noreply.github.com>
2025-09-29 15:22:44 +0800

    feat(learner): add dynamic`learning` content data (#231)

--- apps/learner/src/routes/(protected)/(core)/learning/+page.server.ts
+++ apps/learner/src/routes/(protected)/(core)/learning/+page.server.ts
@@ -1,22 +1,49 @@
+import { redirect } from '@sveltejs/kit';
+
+import { type CollectionModel, db, type TagModel } from '$lib/server/db';
+
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async (event) => {
+  const logger = event.locals.logger.child({ handler: 'page_learning' });
+
+  const { user } = event.locals.session;
+  if (!user) {
+    logger.warn('User not authenticated');
+    throw redirect(303, '/login');
+  }
+
+  interface CollectionWithTags extends Pick<CollectionModel, 'id' | 'title' | 'type'> {
+    numberOfPodcasts: number;
+    tags: Pick<TagModel, 'code' | 'label'>[];
+  }
+
+  const collections = await db.$queryRaw<CollectionWithTags[]>`
+    SELECT
+      c.id AS id,
+      c.title AS title,
+      c.type AS type,
+      COUNT(DISTINCT lj.id) AS "numberOfPodcasts",
+      COALESCE(
+        (
+          SELECT JSON_AGG(
+                  JSONB_BUILD_OBJECT('code', t.code, 'label', t.label)
+                  ORDER BY t.created_at
+                )
+          FROM collection_tags ct
+          INNER JOIN tags t ON t.id = ct.tag_id
+          WHERE ct.collection_id = c.id
+        ),
+        '[]'
+      ) AS tags
+    FROM learning_journeys lj
+    INNER JOIN learning_units lu ON lu.id = lj.learning_unit_id
+    INNER JOIN collections c ON c.id = lu.collection_id
+    WHERE lj.user_id = ${user.id}
+    GROUP BY c.id;
+  `;
+
   return {
-    collections: [
-      {
-        id: 1,
-        title: 'Learn to use AI',
-        type: 'AI',
-        tags: [{ code: 'AI', label: 'Artificial Intelligence' }],
-        numberofpodcasts: 8,
-      },
-      {
-        id: 2,
-        title: 'SEN peer support',
-        type: 'SEN',
-        tags: [{ code: 'SEN', label: 'Special Educational Needs' }],
-        numberofpodcasts: 12,
-      },
-    ],
+    collections,
   };
 };
