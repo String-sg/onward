01dcbc5c82980c38e595a7ec1193c1d4682086af
Kelly Lim <kellylim.06@gmail.com>
2025-09-29 14:48:02 +0800

    update routing from /usercollection/[id] to /learning/collection/[id]

--- apps/learner/src/routes/(protected)/(core)/learning/+page.svelte
+++ apps/learner/src/routes/(protected)/(core)/learning/+page.svelte
@@ -13,7 +13,7 @@
   <div class="grid grid-cols-1 gap-3 md:grid-cols-2">
     {#each data.collections as collection (collection.id)}
       <Collection
-        to="/collection/{collection.id}"
+        to="/learning/collection/{collection.id}"
         title={collection.title}
         type={collection.type}
         tags={collection.tags.map((t) => ({
