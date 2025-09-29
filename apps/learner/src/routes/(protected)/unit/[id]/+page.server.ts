664cb7453656e4333917e1a59c58f5053d27a79e
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-09-29 14:42:48 +0800

    feat(learner): add error handling for learning unit

--- apps/learner/src/routes/(protected)/unit/[id]/+page.server.ts
+++ apps/learner/src/routes/(protected)/unit/[id]/+page.server.ts
@@ -1,12 +1,19 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
-import { db } from '$lib/server/db';
+import { db, type LearningUnitFindUniqueArgs, type LearningUnitGetPayload } from '$lib/server/db';
 
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
-  const learningUnit = await db.learningUnit.findUnique({
-    where: { id: BigInt(params.id) },
+export const load: PageServerLoad = async (event) => {
+  const logger = event.locals.logger.child({ handler: 'page_learning_unit' });
+
+  const { user } = event.locals.session;
+  if (!user) {
+    logger.warn('User not authenticated');
+    return redirect(303, '/login');
+  }
+
+  const learningUnitArgs = {
     select: {
       id: true,
       tags: {
@@ -25,20 +32,35 @@ export const load: PageServerLoad = async ({ params }) => {
       createdAt: true,
       createdBy: true,
     },
-  });
+    where: { id: BigInt(event.params.id) },
+  } satisfies LearningUnitFindUniqueArgs;
+
+  let learningUnit: LearningUnitGetPayload<typeof learningUnitArgs> | null;
+
+  try {
+    learningUnit = await db.learningUnit.findUnique(learningUnitArgs);
+  } catch (err) {
+    logger.error({ err }, 'Failed to retrieve learning unit');
+    throw error(500);
+  }
 
   if (!learningUnit) {
     throw error(404);
   }
 
-  const isQuizAvailable =
-    (await db.questionAnswer.count({
-      where: {
-        learningUnitId: BigInt(params.id),
-      },
-    })) > 0;
+  let isQuizAvailable: boolean;
 
-  console.log(isQuizAvailable);
+  try {
+    isQuizAvailable =
+      (await db.questionAnswer.count({
+        where: {
+          learningUnitId: BigInt(event.params.id),
+        },
+      })) > 0;
+  } catch (err) {
+    logger.error({ err }, 'Failed to retrieve quiz records');
+    throw error(500);
+  }
 
   return {
     id: learningUnit.id,
