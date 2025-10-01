e5c453f2977063b4229800e0876edff6229d719e
Kelly Lim <kellylim.06@gmail.com>
2025-10-01 17:33:47 +0800

    feat(learner): update error page for 404 and !404

--- apps/learner/src/routes/+error.svelte
+++ apps/learner/src/routes/+error.svelte
@@ -7,17 +7,24 @@
 <main
   class="relative mx-auto flex min-h-svh max-w-5xl flex-col items-center justify-center gap-y-4 px-4 py-3"
 >
-  <div class="flex flex-col items-center gap-y-1">
-    <h1 class="text-center text-3xl font-bold">
-      {is404 ? 'Page not found' : 'Something went wrong'}
-    </h1>
+  <h1 class="text-center text-2xl font-semibold">
+    {is404 ? 'Page not found' : 'Something went wrong'}
+  </h1>
 
-    <p class="text-center text-sm text-slate-500">
-      {is404 ? 'The page you are looking for does not exist.' : 'Please try again later.'}
+  <enhanced:img src="$lib/assets/desert.png?w=200" alt="Page not found" />
+
+  <div class="flex flex-col items-center gap-y-8">
+    <a href="/" class="w-64 rounded-full bg-slate-950 px-4 py-3 text-center text-sm text-white">
+      Go to Home
+    </a>
+
+    <p class="max-w-xs text-center text-slate-900">
+      {#if is404}
+        The page you are looking for does not exist.
+      {:else}
+        If the problem persists, please contact the
+        <a href="mailto:support@example.com" class="text-slate-900 underline">IT team</a>.
+      {/if}
     </p>
   </div>
-
-  <a href="/" class="rounded-full bg-slate-950 px-4 py-3 text-center text-sm text-white">
-    Go to Home
-  </a>
 </main>
