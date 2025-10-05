1756b66d3d9cfc89995fb176ae2c1a9cc2e6bea8
santosral <sralphian029@gmail.com>
2025-10-05 18:06:19 +0800

    feat: create Dialogues table

--- apps/learner/prisma/migrations/20250929001220_add_token_count_to_threads_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."messages" ADD COLUMN     "token_count" INTEGER NOT NULL DEFAULT 0;
--- apps/learner/prisma/migrations/20251001174456_add_total_tokens_to_threads_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."threads" ADD COLUMN     "total_tokens" INTEGER NOT NULL DEFAULT 0;
