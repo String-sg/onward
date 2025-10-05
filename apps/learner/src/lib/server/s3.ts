999e0bf6a70c39d1eb1cdebd24d12a290913cab3
YimingIsCOLD <yimingpeh@gmail.com>
2025-10-05 13:29:18 +0800

    feat: add S3 proxy endpoint for podcasts

--- apps/learner/src/lib/server/s3.ts
+++ apps/learner/src/lib/server/s3.ts
@@ -1,4 +1,12 @@
-import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3';
+import { Readable } from 'node:stream';
+
+import {
+  GetObjectCommand,
+  NoSuchKey,
+  NotFound,
+  S3Client,
+  S3ServiceException,
+} from '@aws-sdk/client-s3';
 
 import { env } from '$env/dynamic/private';
 
@@ -18,6 +26,83 @@ const client =
       })
     : new S3Client();
 
-export const listObjects = async () => {
-  return await client.send(new ListObjectsV2Command({ Bucket: BUCKET }));
-};
+/**
+ * Retrieves an object from S3 with the given key. If the object does not exist, `null` is returned.
+ * If the range is not satisfiable, a {@link RangeNotSatisfiableError} is thrown.
+ *
+ * @param key - The key of the object to get.
+ * @param opts.range - An optional range to retrieve a portion of the object.
+ * @returns The object from S3, or `null` if the object does not exist.
+ */
+export async function getPodcastObject(
+  key: string,
+  opts: { range?: string | null } = {},
+): Promise<{
+  stream: ReadableStream;
+  headers: Record<string, string>;
+} | null> {
+  try {
+    const cmd = new GetObjectCommand({ Bucket: BUCKET, Key: key, Range: opts.range ?? undefined });
+    const res = await client.send(cmd);
+
+    if (!res.Body || !(res.Body instanceof Readable)) {
+      return null;
+    }
+
+    const stream = Readable.toWeb(res.Body);
+    if (!(stream instanceof ReadableStream)) {
+      return null;
+    }
+
+    const headers: Record<string, string> = {};
+    if (res.ContentType) {
+      headers['Content-Type'] = res.ContentType;
+    }
+    if (res.ContentLength) {
+      headers['Content-Length'] = String(res.ContentLength);
+    }
+    if (res.AcceptRanges) {
+      headers['Accept-Ranges'] = res.AcceptRanges;
+    }
+    if (res.ContentRange) {
+      headers['Content-Range'] = res.ContentRange;
+    }
+    if (res.LastModified) {
+      headers['Last-Modified'] = res.LastModified.toUTCString();
+    }
+    if (res.ETag) {
+      headers['ETag'] = res.ETag;
+    }
+
+    return { stream, headers };
+  } catch (err) {
+    if (err instanceof NoSuchKey || err instanceof NotFound) {
+      return null;
+    }
+    if (err instanceof S3ServiceException && err.$metadata.httpStatusCode === 416) {
+      throw new RangeNotSatisfiableError();
+    }
+
+    throw err;
+  }
+}
+
+/**
+ * The base class for all S3 errors.
+ */
+export class BaseError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    this.name = this.constructor.name;
+  }
+}
+
+/**
+ * Thrown when a requested range is not satisfiable.
+ */
+export class RangeNotSatisfiableError extends BaseError {
+  constructor() {
+    super('Requested range not satisfiable');
+  }
+}
