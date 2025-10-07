50a705c2006c35382ed298492f117284d377716f
Kelly Lim <kellylim.06@gmail.com>
2025-10-07 17:45:13 +0800

    feat(learner): rename and move logger into if block

--- src/routes/(protected)/(core)/+layout.server.ts
+++ src/routes/(protected)/(core)/+layout.server.ts
@@ -3,10 +3,10 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-  const logger = event.locals.logger.child({ handler: 'layout_root' });
-
   const { user } = event.locals.session;
   if (!user) {
+    const logger = event.locals.logger.child({ handler: 'layout_protected_core' });
+
     logger.warn('User not authenticated');
     return redirect(303, '/login');
   }
