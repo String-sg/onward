0e9931ddcf10cd7d9f0f46e39639c1f11379008c
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-10-08 14:05:56 +0800

    feat(learner): add font-geist-mono to css

--- src/routes/(protected)/(core)/+layout.svelte
+++ src/routes/(protected)/(core)/+layout.svelte
@@ -26,7 +26,10 @@
   <div class="mx-auto w-full max-w-5xl px-4 py-3">
     <div class="flex flex-col gap-y-4">
       <div class="flex items-center justify-between px-2">
-        <span class="text-2xl font-semibold">glow</span>
+        <div class="flex items-center gap-x-1">
+          {@render logo()}
+          <span class="font-mono text-xl font-bold">glow</span>
+        </div>
         <a
           href="/profile"
           aria-label="Profile"
@@ -110,3 +113,58 @@
 <div bind:this={target} class="absolute inset-x-0 top-0 h-px"></div>
 
 {@render children()}
+
+{#snippet logo()}
+  <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M16.0034 6.68726L17.7358 14.2625L25.3345 16.0007H25.3267L17.7358 17.7371L16.0034 25.3123V25.3337L16.0005 25.323L15.9985 25.3337V25.3123L14.2661 17.738L6.67236 16.0007H6.66846L14.2661 14.2625L15.9985 6.68726V6.66675L16.0005 6.67651L16.0034 6.66675V6.68726Z"
+      fill="currentColor"
+    />
+    <ellipse cx="15.9994" cy="2.8485" rx="0.848546" ry="0.848502" fill="currentColor" />
+    <ellipse cx="15.9994" cy="29.152" rx="0.848546" ry="0.848502" fill="currentColor" />
+    <ellipse
+      cx="29.1515"
+      cy="16.0001"
+      rx="0.848502"
+      ry="0.848546"
+      transform="rotate(90 29.1515 16.0001)"
+      fill="currentColor"
+    />
+    <ellipse
+      cx="2.84872"
+      cy="16.0001"
+      rx="0.848502"
+      ry="0.848546"
+      transform="rotate(90 2.84872 16.0001)"
+      fill="currentColor"
+    />
+    <circle
+      cx="0.848524"
+      cy="0.848524"
+      r="0.848524"
+      transform="matrix(-0.707112 0.707102 -0.707112 -0.707102 26.5024 25.2993)"
+      fill="currentColor"
+    />
+    <circle
+      cx="0.848524"
+      cy="0.848524"
+      r="0.848524"
+      transform="matrix(-0.707112 0.707102 -0.707112 -0.707102 7.90137 6.7002)"
+      fill="currentColor"
+    />
+    <circle
+      cx="0.848524"
+      cy="0.848524"
+      r="0.848524"
+      transform="matrix(-0.707112 -0.707102 0.707111 -0.707102 6.70068 26.5)"
+      fill="currentColor"
+    />
+    <circle
+      cx="0.848524"
+      cy="0.848524"
+      r="0.848524"
+      transform="matrix(-0.707112 -0.707102 0.707111 -0.707102 25.2988 7.90088)"
+      fill="currentColor"
+    />
+  </svg>
+{/snippet}
