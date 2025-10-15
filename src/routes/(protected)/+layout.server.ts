595182f35f018d2a7e680ff6efaa3a2d730c7404
santosral <sralphian029@gmail.com>
2025-10-16 00:11:46 +0800

    fix: move the user name state into root layout to persist in all pages

--- src/routes/(protected)/+layout.server.ts
+++ src/routes/(protected)/+layout.server.ts
@@ -1,19 +1,17 @@
 import { redirect } from '@sveltejs/kit';
 
-import { getBase64EncodedAvatar } from '$lib/server/cache/index.js';
-
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
   const { user } = event.locals.session;
   if (!user) {
-    const logger = event.locals.logger.child({ handler: 'layout_protected_core' });
+    const logger = event.locals.logger.child({ handler: 'layout_protected' });
 
     logger.warn('User not authenticated');
     return redirect(303, '/login');
   }
 
   return {
-    avatar: await getBase64EncodedAvatar(BigInt(user.id)),
+    username: user.name,
   };
 };
