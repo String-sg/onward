2a8c819880646cc9f590f5c5d7b1c7aa7f897f1f
Yiming <yimingpeh@gmail.com>
2025-10-21 20:41:07 +0800

    feat: update favicon (#385)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
   import '../app.css';
 
+  import faviconPNG from '$lib/assets/favicon.png';
+  import faviconSVG from '$lib/assets/favicon.svg';
+  import faviconLightPNG from '$lib/assets/favicon-light.png';
+  import faviconLightSVG from '$lib/assets/favicon-light.svg';
+
   const { children } = $props();
 
   import { initAnalytics } from '$lib/helpers/index.js';
 
   initAnalytics();
 </script>
+
+<svelte:head>
+  <link
+    rel="icon"
+    type="image/svg+xml"
+    href={faviconLightSVG}
+    media="(prefers-color-scheme: dark)"
+  />
+  <link rel="icon" type="image/svg+xml" href={faviconSVG} media="(prefers-color-scheme: light)" />
+  <link rel="icon" type="image/png" href={faviconLightPNG} media="(prefers-color-scheme: dark)" />
+  <link rel="icon" type="image/png" href={faviconPNG} media="(prefers-color-scheme: light)" />
+</svelte:head>
 
 {@render children()}
