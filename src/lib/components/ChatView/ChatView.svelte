2a3376d310049e418ca1fa6a94b9c9cb267036f5
santosral <sralphian029@gmail.com>
2025-10-28 14:17:57 +0800

    fix: clear the error message when new user query is sent

--- src/lib/components/ChatView/ChatView.svelte
+++ src/lib/components/ChatView/ChatView.svelte
@@ -116,8 +116,11 @@
   };
 
   const handleSendQuery = async () => {
-    if (!query.trim()) return;
+    if (!query.trim()) {
+      return;
+    }
 
+    error = null;
     messages.push({ role: 'USER', content: query });
     isAiTyping = true;
 
@@ -125,6 +128,7 @@
     query = '';
 
     await tick();
+
     if (chatWindow) {
       chatWindow.scrollTo({
         top: chatWindow.scrollHeight,
@@ -145,19 +149,22 @@
 
       if (!response.ok) {
         isAiTyping = false;
+
         if (response.status === 401) {
           goto('/login');
           return;
         }
 
         error = DEFAULT_ERROR_MESSAGE;
-        return;
+
+        throw new Error('Response not ok');
       }
 
       if (!response.body) {
         isAiTyping = false;
         error = DEFAULT_ERROR_MESSAGE;
-        return;
+
+        throw new Error('Response body is missing');
       }
 
       const reader = response.body.getReader();
@@ -169,6 +176,7 @@
       messages.push({ role: 'ASSISTANT', content: '' });
 
       await tick();
+
       if (chatWindow) {
         chatWindow.scrollTo({
           top: chatWindow.scrollHeight,
@@ -201,12 +209,10 @@
 
           if (payload.type === 'error') {
             if (payload.message === 'Max number of tokens has been reached.') {
-              error = 'Max tokens has been reached, please clear your chat.';
-              break;
+              throw new Error('Max tokens has been reached, please clear your chat.');
             } else {
               messages.pop();
-              error = DEFAULT_ERROR_MESSAGE;
-              break;
+              throw new Error(DEFAULT_ERROR_MESSAGE);
             }
           }
 
@@ -224,6 +230,15 @@
       }
     } catch {
       error = DEFAULT_ERROR_MESSAGE;
+
+      await tick();
+
+      if (chatWindow) {
+        chatWindow.scrollTo({
+          top: chatWindow.scrollHeight,
+          behavior: 'smooth',
+        });
+      }
     }
   };
 
@@ -257,6 +272,8 @@
       messages = [];
     } catch (err) {
       console.error(err);
+    } finally {
+      error = null;
     }
   };
 </script>
