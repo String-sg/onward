78cf6e5d33053ad060aca7812be658b971cddc34
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2025-11-20 15:48:32 +0800

    feat: add mock quiz failed modal

--- src/routes/(protected)/unit/[id]/quiz/+page.svelte
+++ src/routes/(protected)/unit/[id]/quiz/+page.svelte
@@ -24,6 +24,8 @@
   let selectedOptionIndex = $state(-1);
   let isFeedbackModalOpen = $state(false);
   let isCompletionModalOpen = $state(false);
+  let isQuizFailedModalOpen = $state(false);
+  let correctAnswers = $state(0);
 
   const currentQuestionAnswer = $derived(data.questionAnswers[currentQuestionAnswerIndex]);
   const isCorrectAnswer = $derived(selectedOptionIndex === currentQuestionAnswer.answer);
@@ -34,7 +36,6 @@
   const player = Player.get();
   const isWithinViewport = new IsWithinViewport(() => target);
 
-  let correctAnswers = 0;
   let score = 0;
   let isQuizPassed = false;
 
@@ -269,6 +270,31 @@
           class="inline-flex w-full cursor-pointer items-center justify-center rounded-full px-3.75 py-2.75 font-medium hover:bg-slate-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-950 focus-visible:outline-dashed"
         >
           Check my profile
+        </a>
+      </div>
+    </div>
+  </div>
+</Modal>
+
+<Modal isopen={isQuizFailedModalOpen} onclose={noop} variant="light">
+  <div class="mx-auto flex min-h-svh max-w-5xl flex-col items-center justify-center px-4 py-6">
+    <div class="relative mt-2.5 pb-75">
+      <enhanced:img
+        src="$lib/assets/page-not-found.png?w=584"
+        alt="Page not found"
+        class="w-[292px]"
+      />
+
+      <div
+        class="absolute inset-x-0 bottom-0 flex flex-col items-center justify-center gap-y-6 text-xl font-medium"
+      >
+        <span>Failed the quiz</span>
+        <span>{correctAnswers}/{data.questionAnswers.length}</span>
+        <a
+          href="/unit/{params.id}"
+          class="inline-flex w-full cursor-pointer items-center justify-center rounded-full px-3.75 py-2.75 hover:bg-slate-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-950 focus-visible:outline-dashed"
+        >
+          Retry
         </a>
       </div>
     </div>
