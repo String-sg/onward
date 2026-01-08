2f832de6aa7349be086511a3c8dfba0d0031bf8f
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2026-01-08 14:39:09 +0800

    feat: enhance masthead to match latest requirement

--- src/lib/components/Masthead/Masthead.svelte
+++ src/lib/components/Masthead/Masthead.svelte
@@ -11,7 +11,8 @@
     <div class="flex items-center justify-start gap-1 bg-gray-100 px-4 py-1">
       {@render sgCrest()}
       <div class="flex flex-wrap items-center gap-1 text-[0.625rem] sm:text-xs">
-        <span>A Singapore Government Agency Website</span>
+        <span>A Singapore Government Agency Website. Beware of government impersonation scams.</span
+        >
         <button
           class="flex cursor-pointer items-center text-[#1a56e5] focus:outline-[#1a56e5]"
           tabIndex={0}
@@ -54,6 +55,16 @@
                 {@render lock()}
                 ) or https:// as an added precaution. Share sensitive information only on official, secure
                 websites.
+              </div>
+            </div>
+          </div>
+          <div class="flex max-w-[32rem] gap-2">
+            {@render infoIcon()}
+            <div>
+              <div class="font-semibold">Scam alert</div>
+              <div>
+                Government officers will never ask you to send money or share your details over the
+                phone. <b>When unsure, hang up and call Scamshield at 1799.</b>
               </div>
             </div>
           </div>
@@ -160,6 +171,22 @@
       fill-rule="evenodd"
       clip-rule="evenodd"
       d="M5.83331 8.33317H5.41665C4.26605 8.33317 3.33331 9.26591 3.33331 10.4165V16.2498C3.33331 17.4004 4.26605 18.3332 5.41665 18.3332H14.5833C15.7339 18.3332 16.6666 17.4004 16.6666 16.2498V10.4165C16.6666 9.26591 15.7339 8.33317 14.5833 8.33317H14.1666V5.83317C14.1666 3.53198 12.3012 1.6665 9.99998 1.6665C7.69879 1.6665 5.83331 3.53198 5.83331 5.83317V8.33317ZM7.49998 8.33317H12.5V5.83317C12.5 4.45246 11.3807 3.33317 9.99998 3.33317C8.61927 3.33317 7.49998 4.45246 7.49998 5.83317V8.33317Z"
+      fill="currentColor"
+    />
+  </svg>
+{/snippet}
+
+{#snippet infoIcon()}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="inline h-5 w-5 shrink-0"
+    viewBox="0 0 16 17"
+    fill="none"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8.33342 1.3335C12.0153 1.3335 15.0001 4.31826 15.0001 8.00016C15.0001 11.6821 12.0153 14.6668 8.33342 14.6668C4.65152 14.6668 1.66675 11.6821 1.66675 8.00016C1.66675 4.31826 4.65152 1.3335 8.33342 1.3335ZM8.33342 10.0002C7.96523 10.0002 7.66675 10.2986 7.66675 10.6668C7.66675 11.035 7.96523 11.3335 8.33342 11.3335C8.7016 11.3335 9.00008 11.035 9.00008 10.6668C9.00008 10.2986 8.7016 10.0002 8.33342 10.0002ZM8.33342 4.68311C8.01125 4.68311 7.75008 4.94492 7.75008 5.26709V8.66683C7.75026 8.98885 8.01136 9.25016 8.33342 9.25016C8.65547 9.25016 8.91657 8.98885 8.91675 8.66683V5.26709C8.91675 4.94492 8.65558 4.68311 8.33342 4.68311Z"
       fill="currentColor"
     />
   </svg>
