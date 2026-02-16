16128ba31ef04b5ae5844011e1b1730b883ee328
santosral <sralphian029@gmail.com>
2026-02-16 11:51:58 +0800

    feat: update user_admins table

--- prisma/migrations/20260216034930_add_user_admins_table/migration.sql
+++ prisma/migrations/20260216034930_add_user_admins_table/migration.sql
@@ -3,9 +3,9 @@ CREATE TABLE "user_admins" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v7(),
     "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "name" VARCHAR(255),
+    "name" VARCHAR(255) NOT NULL,
     "email" TEXT NOT NULL,
-    "google_provider_id" TEXT,
+    "google_provider_id" TEXT NOT NULL,
     "is_active" BOOLEAN NOT NULL DEFAULT true,
 
     CONSTRAINT "user_admins_pkey" PRIMARY KEY ("id")
