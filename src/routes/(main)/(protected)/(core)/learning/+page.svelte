a58cdd3681a93517d936c259e61a8c6cbe2b0da5
Nicholas Lim <nicholas_LIM@tech.gov.sg>
2026-03-02 15:29:50 +0800

    feat: update learning page to remove collection and show bites directly

--- src/routes/(main)/(protected)/(core)/learning/+page.svelte
+++ src/routes/(main)/(protected)/(core)/learning/+page.svelte
@@ -1,27 +1,53 @@
 <script lang="ts">
-  import { Collection } from '$lib/components/Collection/index.js';
   import { EmptyStateView } from '$lib/components/EmptyStateView/index.js';
+  import { LearningUnit } from '$lib/components/LearningUnit';
 
   const { data } = $props();
 </script>
 
 <main class="relative mx-auto flex min-h-svh max-w-5xl flex-col gap-y-3 px-4 pt-43 pb-28">
-  {#if data.collections.length > 0}
-    <div class="px-2">
-      <span class="text-xl font-semibold">Your learnings</span>
-    </div>
+  {#if data.journeys.inProgress.length > 0}
+    <div class="flex flex-col gap-y-3">
+      <div class="px-2">
+        <span class="text-xl font-medium">In Progress</span>
+      </div>
 
-    <div class="grid grid-cols-1 gap-3 md:grid-cols-2">
-      {#each data.collections as collection (collection.id)}
-        <Collection
-          to={`/learning/collection/${collection.id}`}
-          title={collection.title}
-          type={collection.tagCode}
-          numberofpodcasts={collection.numberOfPodcasts}
-        />
-      {/each}
+      <div class="grid grid-cols-1 gap-3 md:grid-cols-2">
+        {#each data.journeys.inProgress as journey (journey.id)}
+          <LearningUnit
+            to="/unit/{journey.unitId}"
+            tags={journey.tags}
+            title={journey.title}
+            createdat={journey.createdAt}
+            createdby={journey.createdBy}
+            status={journey.status}
+          />
+        {/each}
+      </div>
     </div>
-  {:else}
+  {/if}
+
+  {#if data.journeys.isCompleted.length > 0}
+    <div class="flex flex-col gap-y-3">
+      <div class="px-2">
+        <span class="text-xl font-medium">Completed</span>
+      </div>
+
+      <div class="grid grid-cols-1 gap-3 md:grid-cols-2">
+        {#each data.journeys.isCompleted as journey (journey.id)}
+          <LearningUnit
+            to="/unit/{journey.unitId}"
+            tags={journey.tags}
+            title={journey.title}
+            createdat={journey.createdAt}
+            createdby={journey.createdBy}
+          />
+        {/each}
+      </div>
+    </div>
+  {/if}
+
+  {#if data.journeys.inProgress.length === 0 && data.journeys.isCompleted.length === 0}
     <div class="mt-8 flex flex-col items-center gap-y-5">
       <EmptyStateView username={data.username} imagealt="No learning journeys found" />
     </div>
